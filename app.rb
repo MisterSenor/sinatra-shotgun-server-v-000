dbdec78846c4267a7720469c07e9889e0c1e5866
Nathan  Visak <nathan.visak@gmail.com>
2018-08-26 20:05:07 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,7 +3,9 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do
-    "Welcome to your app!!!! Aren't you excited to learn about building web apps? \n \n Is Shotgun working?"
+    "Welcome to your app!!!! Aren't you excited to learn about building web apps?
+
+    Is Shotgun working?"
   end
 
 end
